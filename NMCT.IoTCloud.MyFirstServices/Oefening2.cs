50ed2995afc20fa7059e5769d8af8e69adecf65a
BrentDelarue <brent.delarue@student.howest.be>
2018-10-03 14:20:35 +0200

    History added

--- NMCT.IoTCloud.MyFirstServices/Oefening2.cs
+++ NMCT.IoTCloud.MyFirstServices/Oefening2.cs
@@ -12,21 +12,30 @@ using Newtonsoft.Json;
 
 namespace NMCT.IoTCloud.MyFirstServices
 {
-    public static class Oefening2
-    {
-        [FunctionName("Oefening2")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "rekenmachine/delen/{getal1}/{getal2}")]HttpRequest req, int getal1, int getal2, ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+//    public static class Oefening2
+//    {
+//        [FunctionName("Oefening2")]
+//        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "rekenmachine/delen/{getal1}/{getal2}")]HttpRequest req, int getal1, int getal2, ILogger log)
+//        {
+//            try
+//            {
+//                log.LogInformation("C# HTTP trigger function processed a request.");
+//                if (getal2 == 0)
+//                {
+//                    return new BadRequestResult();
+//                }
+//                int get1 = getal1;
+//                int get2 = getal2;
 
-            int get1 = getal1;
-            int get2 = getal2;
+//                float deling = get1 / get2;
+//            }
+//            catch (Exception)
+//            {
+//                return new OkObjectResult($"De deling van {get1} en {get2} = {deling}");
+//            }
 
-            float deling = get1 / get2;
 
-            return get2 != 0
-                ? (ActionResult)new OkObjectResult($"De deling van {get1} en {get2} = {deling}")
-                : new BadRequestObjectResult("Please pass two numbers on the query string or in the request body that are not 0.");
-        }
-    }
+//            return new OkObjectResult($"De deling van {get1} en {get2} = {deling}");
+//         }
+//    }
 }
